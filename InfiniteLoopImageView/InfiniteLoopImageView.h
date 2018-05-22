1137ca6747e4f3b7e5602eafb78e38e64125e70d
Hinomori Hiroya <hiroya.hinomori@nhn-comico.com>
2018-05-22 21:36:33 +0900

    Initial Commit

---
+++ InfiniteLoopImageView/InfiniteLoopImageView.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteLoopImageView.h
+//  InfiniteLoopImageView
+//
+//  Created by Hinomori Hiroya on 2018/05/22.
+//  Copyright © 2018年 Hinomori Hiroya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteLoopImageView.
+FOUNDATION_EXPORT double InfiniteLoopImageViewVersionNumber;
+
+//! Project version string for InfiniteLoopImageView.
+FOUNDATION_EXPORT const unsigned char InfiniteLoopImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteLoopImageView/PublicHeader.h>
+
+
